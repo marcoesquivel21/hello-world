1e5e9c68839cbc94c74dfdabc17526628b39301b
marcoesquivel21 <31970874+marcoesquivel21@users.noreply.github.com>
2018-05-19 18:53:51 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*Marco Esquivel
+* First github Project
+* 5/19/2018
+* Hello World - java
+*/
+public class HelloWorld{
+	public static void main (String [] args) {
+		System.out.println("Hello world welcome to my github!");
+	}//end of main method
+}//end of class
